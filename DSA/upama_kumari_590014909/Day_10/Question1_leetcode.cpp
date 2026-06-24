74979af64ef25b689825ec8109dfc88f6772d1dc
upma77 <upmakumarikaimur@gmail.com>
2026-06-24 16:16:06 +0530

    update file

--- DSA/upama_kumari_590014909/Day_10/Question1_leetcode.cpp
+++ DSA/upama_kumari_590014909/Day_10/Question1_leetcode.cpp
@@ -1,29 +1,35 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int matchingstring(string haystack,string needle){
-
+int matchingstring(string haystack, string needle)
+{
     int m = haystack.length();
     int n = needle.length();
 
-    for(int i=0;i<=m-n;i++){
-        for(int j=0;i<n;j++){
-            if(haystack[i+j]!=needle[j]){
+    for(int i = 0; i <= m - n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(haystack[i + j] != needle[j])
+            {
                 break;
             }
-            if(j==n-1){
+
+            if(j == n - 1)
+            {
                 return i;
-            } 
-        }   
+            }
+        }
     }
+
     return -1;
 }
-int main(){
-    string haystack="sadofsad";
-    string needle = "sad";
-    cout<<matchingstring(haystack,needle);
+
+int main()
+{
+    string haystack = "hello";
+    string needle = "ll";
+
+    cout << matchingstring(haystack, needle);
 }
-
-
-
